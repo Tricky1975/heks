1f8d7a1cca95d1b5d0de38c6571b77a0c01f8dba
Tricky1975 <facebook@tbbs.nl>
2019-01-24 16:13:10 +0100

    Preps

--- heks.c
+++ heks.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
 //                      0  1  2  3   4  5  6  7   8  9  A  B   C  D  E  F  
 //                      1         2        3         4         5         6         7
 //             123456789012345678901234678901234567890123456789012345678901234567890123456789
 #define basis "........ .. .. .. ..  .. .. .. ..  .. .. .. ..  .. .. .. ..  ................"
 
+int fails = 0;
+
+int hpos(i){ return (i*3)+(int)floor(i/4)+10; }
 
 void showfile(char* file){
+	char ln[18];
+	char ch[2]; 
 	printf("Reading: %s\n",file);
+	FILE * bt = fopen(file,"rb");
+	if (bt==NULL) { printf("= Error opening file\n"); fails++; return; }
+	
+	fclose(bt);
 }
 
 
@@ -18,5 +30,9 @@ int main( int argc, char *argv[] )  {
 		return 0;
 	}
 	for (int i=1;i<argc;i++) showfile(argv[i]);
+	if (fails==1) 
+		printf("\t1 file failed to load\n");
+	else if (fails>1)
+		printf("\t%d files failed to load\n",fails);
 	return 0;
 }
